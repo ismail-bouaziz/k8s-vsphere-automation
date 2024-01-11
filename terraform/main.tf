0855e2c7dc14a936c3da3bd1e943f0e1326e43e7
ismail-bouaziz <ismailboaziz@gmail.com>
2024-01-11 21:29:02 +0100

    add inplicit depen

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,9 @@
-resource "local_file" "pet"{
-    filename = "/home/Ismail-Bouaziz/Documents/k8s-vsphere-automation/terraform/pets.txt"
-    content = "We love pets!"
-    file_permission = "700"
+
+resource "local_file" "pet" {
+  filename        = "/home/Ismail-Bouaziz/Documents/k8s-vsphere-automation/terraform/${random_pet.petname.id}.txt"
+  content         = "We love pets!"
+  file_permission = "700"
+}
+resource "random_pet" "petname" { 
+   length = 1
 }
