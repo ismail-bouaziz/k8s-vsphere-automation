46538595bc641f8106c81662e65498bc311e07fc
ismail-bouaziz <ismailboaziz@gmail.com>
2024-06-10 23:13:30 +0100

    Add Cluster installation

--- terraform/main.tf
+++
@@ -1,9 +0,0 @@
-
-resource "local_file" "pet" {
-  filename        = "/home/Ismail-Bouaziz/Documents/k8s-vsphere-automation/terraform/${random_pet.petname.id}.txt"
-  content         = "We love pets!"
-  file_permission = "700"
-}
-resource "random_pet" "petname" { 
-   length = 1
-}
